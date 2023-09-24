f39f38c57e5ea7bfa6e7b50b69a1559b45d6565e
oli <oli1412001@gmail.com>
2023-09-24 10:34:11 +0600

    number 1 finished

--- assignment.js
+++ assignment.js
@@ -1,1 +1,15 @@
-console.log('hi');
+// number : 1
+function cubeNumber(number) {
+    if(typeof number !== 'number'){
+        const msg =  console.log('Please insert a positive integer number');
+        return msg;
+    }else{
+        const cube = number * number * number;
+        return cube;
+    }  
+}
+const positiveDigit = '2';
+const result = cubeNumber(positiveDigit);
+console.log(result);
+
+// number : 2
